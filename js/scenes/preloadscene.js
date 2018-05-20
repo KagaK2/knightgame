857418a2591e648e15ea397b501f2ff51923ad18
KagaK2 <KhanhPhan2512@gmail.com>
2018-05-20 13:27:34 +0300

    package json was a mistake

--- js/scenes/preloadscene.js
+++ js/scenes/preloadscene.js
@@ -2,17 +2,17 @@ class PreloadScene extends Phaser.Scene {
     constructor() {
       super({key: 'PreloadScene'});
     }
-  
+
     preload ()
     {
-        this.load.image('background','../assets/bkg.png');
-        this.load.image('ground','../assets/platform.png');
-        this.load.spritesheet('health','../assets/plates.png',{ frameWidth: 48, frameHeight: 48 });
-        this.load.spritesheet('pawn','../assets/Ghost.png', { frameHeight: 78, frameWidth: 59 });
-        this.load.spritesheet('dude', '../assets/dude.png', { frameWidth: 71.5, frameHeight: 80 });
-        this.load.spritesheet('jumpdude', '../assets/jump.png',{frameWidth: 66, frameHeight: 79 });
+        this.load.image('background','../knightgame/assets/bkg.png');
+        this.load.image('ground','../knightgame/assets/platform.png');
+        this.load.spritesheet('health','../knightgame/assets/plates.png',{ frameWidth: 48, frameHeight: 48 });
+        this.load.spritesheet('pawn','../knightgame/assets/Ghost.png', { frameHeight: 78, frameWidth: 59 });
+        this.load.spritesheet('dude', '../knightgame/assets/dude.png', { frameWidth: 71.5, frameHeight: 80 });
+        this.load.spritesheet('jumpdude', '../knightgame/assets/jump.png',{frameWidth: 66, frameHeight: 79 });
     }
-    
+
     create() {
       this.scene.start('PlayScene');
     }
